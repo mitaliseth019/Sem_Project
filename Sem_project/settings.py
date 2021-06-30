26321ade78be7e335eb5268d93f937bdd4dfbf0e
Mitali <sethmitali007@gmail.com>
2021-07-01 00:21:32 +0530

    deploy

--- Sem_project/settings.py
+++ Sem_project/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    #'django.contrib.sites',
+    'django.contrib.sites',
     #local
     'crispy_forms',
     'phone_field',
